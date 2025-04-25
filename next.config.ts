0bf2d724725f7b2536b22747269801e2f7dde7ec
NayanCod <kumarnayab6@gmail.com>
2025-04-25 10:38:34 +0530

    created courses page

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["api.microlink.io"],
+    domains: ["api.microlink.io", "images.unsplash.com"],
   },
 };
 
